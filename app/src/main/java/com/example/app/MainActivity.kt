24481ddf1ffabd3adea2b618d006bba303f02147
woogun <bome519@naver.com>
2023-02-28 22:51:50 +0900

    액티비티 LifeCycle 동작 과정 확인

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "MainActivity"
+
+// 액티비티 : 사용자와 상호작용하는 UI
+// inflate : Layout의 ID를 받아 Layout 구현부를 widget으로 바꾸는 과정
 class MainActivity : AppCompatActivity() {
+    // 데이터를 초기화하는 작업
+    // Layout을 inflate 하는 과정
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        Log.d(TAG, "onCreate")
     }
+
+    // on으로 시작하는 메서드(name convention) : 어떠한 이벤트를 알려주는 메서드
+    // 콜백 함수 : 어떠한 이벤트가 발생하고 호출되는 함수
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
 }
