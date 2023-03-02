1db8a2427c6e60eeb595f288aff2101dc468c666
woogun <bome519@naver.com>
2023-03-02 21:16:12 +0900

    Feat: Splash 액티비티 구현

---
+++ app/src/main/java/com/example/app/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        // 액티비티 간의 이동 할 때 Intent 객체를 사용 (현재 액티비티, 이동할 액티비티)
+        // 클래스 참조 : class::java
+        startActivity(Intent(this, MainActivity::class.java))
+        // splash 액티비티 종료
+        finish()
+    }
+
+}
