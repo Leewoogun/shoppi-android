ea64321ab3ddcb972efb74addd825b9c0e75be65
woogun <bome519@naver.com>
2023-03-16 00:45:27 +0900

    Feat : category detail 데이터 설계

--- app/src/main/java/com/example/app/model/Title.kt
+++ app/src/main/java/com/example/app/model/Title.kt
@@ -1,6 +1,10 @@
 package com.example.app.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Title(
     val text : String,
-    val iconUrl : String
+    // category detail 영역에서는 iconUrl이 필요없기 때문에 nullable로 처리
+    @SerializedName("icon_url")
+    val iconUrl : String?
 )
