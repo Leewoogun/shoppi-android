127674badb3262d98ec359778f3144b6571e8aac
woogun <bome519@naver.com>
2023-03-19 21:20:48 +0900

    Feat : 중첩 리사이클러뷰 레이아웃 구현
    BindingAdapter에 Glide 라이브러리 circleCrop 속성을 활용하여 원형 ImageView 할당

--- app/src/main/java/com/example/app/ui/common/ImageBindingAdapters.kt
+++ app/src/main/java/com/example/app/ui/common/ImageBindingAdapters.kt
@@ -17,3 +17,20 @@ fun loadImage(view: ImageView, imageUrl: String?) {
             .into(view)
     }
 }
+
+// 이미지 뷰를 원형으로 받아오는 커스텀 함수
+@BindingAdapter("circleImageUrl")
+fun loadCircleImage(view: ImageView, imageUrl: String?) {
+    /*
+      with인자 : Activity or Fragment
+      load인자 : url 주소
+      into인자 : 어떠한 리소스로 이미지 뷰에 할당할 것인가
+      circleCrop : 이미지를 원형으로 렌더링
+     */
+    if (!imageUrl.isNullOrEmpty()) {
+        GlideApp.with(view)
+            .load(imageUrl)
+            .circleCrop()
+            .into(view)
+    }
+}
